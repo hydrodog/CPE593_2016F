47c5cb294efe56205149903be68cf977cb72e4a6
Dov Kruger <dov.kruger@gmail.com>
2016-10-04 16:45:42 -0400

    session06

---
+++ session06/DoubleLinkedList2.java
@@ -0,0 +1,67 @@
+public class DoubleLinkedList {
+	private static class Node { // LinkedList.Node
+    int val;
+		Node next;
+		Node prev;
+	}
+	
+	private Node head, tail;
+	public DoubleLinkedList() {
+
+
+	}
+
+
+	public void addEnd(int v) {
+
+		
+	}
+	public void insert(int i, int v) {
+
+
+	}
+	
+	public void removeEnd() {
+
+
+	}
+	public void removeStart() {
+
+
+
+
+	}
+
+
+	public String toString() {
+
+
+
+
+
+	}
+
+	public int size() {
+
+
+
+	}
+
+	public int get(int i) {
+
+
+
+	}
+
+	public static void main(String[]args) {
+		LinkedList2 a = new LinkedList2();
+		for (int i = 0; i < 10; i++)
+			a.addStart(i);
+		System.out.println(a);
+		for (int i = 0; i < 10; i++)
+			a.addEnd(i);
+		System.out.println(a);
+		
+			
+	}
+}	
---
+++ session06/LinkedList2.java
@@ -0,0 +1,66 @@
+public class LinkedList2 {
+	private static class Node { // LinkedList.Node
+    int val;
+		Node next;
+	}
+	
+	private Node head, tail;
+	public LinkedList2() {
+
+
+	}
+
+
+	public void addEnd(int v) {
+
+		
+	}
+	public void insert(int i, int v) {
+
+
+	}
+	
+	public void removeEnd() {
+
+
+	}
+	public void removeStart() {
+
+
+
+
+	}
+
+
+	public String toString() {
+
+
+
+
+
+	}
+
+	public int size() {
+
+
+
+	}
+
+	public int get(int i) {
+
+
+
+	}
+
+	public static void main(String[]args) {
+		LinkedList2 a = new LinkedList2();
+		for (int i = 0; i < 10; i++)
+			a.addStart(i);
+		System.out.println(a);
+		for (int i = 0; i < 10; i++)
+			a.addEnd(i);
+		System.out.println(a);
+		
+			
+	}
+}	
