18077423ada655a860506aa9213d69fb4d2c0193
Dov Kruger <dov.kruger@gmail.com>
2016-10-05 14:25:54 -0400

    example of reading in text for parsing in C++

---
+++ session06/readintext.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+int main() {
+	string line = cin.getline();
+	istringstream s(line);
+	string cmd;
+	s >> cmd;
+	if (cmd == "ADD_FRONT") {
+
+	}
+}
